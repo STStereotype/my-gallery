18cbce805fbafc6abccafa424d392146953903e0
STStereotype <62321778+STStereotype@users.noreply.github.com>
2022-03-20 15:45:28 +0700

    Создание компонента.
    Был создан компонент "gallery".

--- src/app/my-gallery/my-gallery.module.ts
+++ src/app/my-gallery/my-gallery.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from "@angular/core";
 import {FolderSidebarComponent} from "./folder-sidebar/folder-sidebar.component";
 import {MyGalleryRoutingModule} from "./my-gallery-routing.module";
 import {MyGalleryComponent} from "./my-gallery.component";
+import { GalleryComponent } from './gallery/gallery.component';
 
 
 @NgModule({
   declarations: [
     MyGalleryComponent,
-    FolderSidebarComponent
+    FolderSidebarComponent,
+    GalleryComponent
   ],
   exports: [
     FolderSidebarComponent
