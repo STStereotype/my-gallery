68b564e829810e67536f420c38178f7c7ac1f15b
STStereotype <62321778+STStereotype@users.noreply.github.com>
2022-03-20 15:49:40 +0700

    HTML разметка.
    Была добавлена HTML разметка в компонент "gallery" и созданы переменные.

--- src/app/my-gallery/gallery/gallery.component.ts
+++ src/app/my-gallery/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-gallery',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  currentFolderName!: string;
+  currentFolderSize!: number;
+  numberImagesCurrentFolder!: number;
+  creationDateCurrentFolder!: string;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      console.log(params);
+      this.currentFolderName =  params['name'];
+      this.currentFolderSize = 0;
+      this.numberImagesCurrentFolder = 0;
+      this.creationDateCurrentFolder = "01.01.2000"
+    })
   }
-
 }
